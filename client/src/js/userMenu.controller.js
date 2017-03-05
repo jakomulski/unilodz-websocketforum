e55391f3e67afa1d8c6c2cf43904720e884ccc49
nonecover <adam.jakomulski@gmail.com>
2017-03-05 09:45:20 +0100

    Added structure nature, modified menu items

--- client/src/js/userMenu.controller.js
+++ client/src/js/userMenu.controller.js
@@ -1,36 +1,6 @@
 angular
     .module('app')
-    .controller('UserMenuController', ['$scope', '$document', '$location', function ($scope, $document, $location) {
-
-        $scope.menuStructure = [];
-
-        function menu(title, pos, content, action) {
-            this.pos = pos;
-            this.title = title;
-            this.content = content;
-            this.onopen = action ? action : function () {
-                if ($scope.menuStructure.length > pos) {
-                    $scope.menuStructure[pos] = this;
-                } else {
-                    $scope.menuStructure.push(this);
-                }
-            }
-            this.onexit = function () {
-                var index = $scope.menuStructure.indexOf(this);
-                $scope.menuStructure.splice(index, 1);
-            }
-        }
-
-
-        var sub4 = new menu('sub4', 2, [], function () {});
-        var sub3 = new menu('sub3', 2, [], function () {});
-        var userAccount = new menu('user account', 1, [sub3, sub4]);
-        var homePage = new menu('home page', 1, [], function () {
-            $location.path('/home');
-        });
-
-        var settings = new menu('settings', 0, [homePage, userAccount]);
-
+    .controller('UserMenuController', ['$scope', '$document', '$location', 'userMenuService', function ($scope, $document, $location, userMenuService) {
         $scope.changeContentSize = function (event) {
             var target = event.target;
             var container = target.previousElementSibling;
@@ -47,9 +17,7 @@ angular
                 });
             })
         };
-
-        $scope.openSettings = function () {
-            if ($scope.menuStructure.indexOf(settings) === -1)
-                $scope.menuStructure.unshift(settings);
-        }
+        $scope.menuStructure = userMenuService.menuStructure;
+        $scope.openSettings = userMenuService.openSettings;
+        $scope.openAccount = userMenuService.openAccount;
    }]);
